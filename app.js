4ed10d84e2f467ff12d6b066158a27ede7c5b2ab
Leonardo Manarin <lezera12@gmail.com>
2024-10-24 16:48:23 -0300

    teste

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const { teste } = require("./domains/usuarios/routes/test");
 const { cadastrarUsuario } = require("./domains/usuarios/routes/cadastrar");
 const {verificarUsuario} = require("./domains/usuarios/routes/verificar");
 const { json } =  require('body-parser');
-const con = require("./common/db"); // testando conexao com o banco de dados antes de iniciar a aplicaçao
+//const con = require("./common/db"); // testando conexao com o banco de dados antes de iniciar a aplicaçao
 
 const app = express();
 app.use(json());
 app.use(teste);
 app.use(cadastrarUsuario, verificarUsuario);
 
-app.listen(3000, () => {
-    console.log("Escutando na porta 3000!");
+app.listen(8000, () => {
+    console.log("Escutando na porta 8000!");
 })
 
