199ea1e6d20af3356923fcb5ea2ac3b59e129185
Leonardo Manarin <lezera12@gmail.com>
2024-10-27 00:54:43 -0300

    Final

--- domains/conversoes/routes/inserirConversao.js
+++ domains/conversoes/routes/inserirConversao.js
@@ -4,11 +4,12 @@ const router = Router();
 
 router.post("/api/conversoes/inserir", async (req, res) => {
     function removeSymbol(inputString, symbol) {
-        const regex = new RegExp(symbol, ' ❤️');
+        const regex = new RegExp(symbol, 'g');
         return inputString.replace(regex, '');
     }
     
-    const {usuarioid, moedaid, moedanome, quantidade, precobrl, precousd }= req.body
+    let {usuarioid, moedaid, moedanome, quantidade, precobrl, precousd }= req.body
+    moedanome = removeSymbol(moedanome, ' ❤️' )
     var sql = `INSERT INTO conversoes (usuarioid, moedaid, moedanome, quantidade, precobrl, precousd) VALUES ('${usuarioid}', '${moedaid}', '${moedanome}', '${quantidade}', '${precobrl}', '${precousd}')`;
     con.query(sql, (err, result) => {
         if (err) return res.send(err);
